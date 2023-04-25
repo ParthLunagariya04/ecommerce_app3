26a48152c77197e4d177b4dab6c09c6d6202189c
Skyfall-4 <alphaskyfall03@gmail.com>
2023-04-25 14:17:52 +0530

    test: :white_check_mark: Add test for logout, "account_screen_test.dart"

---
+++ test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -0,0 +1,27 @@
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Cancel logout', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: AccountScreen(),
+        ),
+      ),
+    );
+    final logoutButton = find.text('Logout');
+    expect(logoutButton, findsOneWidget);
+    await tester.tap(logoutButton);
+    await tester.pump();
+    final dialogTitle = find.text('Are you sure?');
+    expect(dialogTitle, findsOneWidget);
+    final cancelButton = find.text('Cancel');
+    expect(cancelButton, findsOneWidget);
+    await tester.tap(cancelButton);
+    await tester.pump();
+    expect(dialogTitle, findsNothing);
+  });
+}
