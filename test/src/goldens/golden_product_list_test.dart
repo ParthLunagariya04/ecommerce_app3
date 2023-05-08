17380440e532764705832806fe7c31d04ad3ebab
Skyfall-4 <alphaskyfall03@gmail.com>
2023-05-08 19:26:13 +0530

    test: :white_check_mark: Golden image test
    Golden image test generate test image, 'product_list.png' is a generated image

---
+++ test/src/goldens/golden_product_list_test.dart
@@ -0,0 +1,18 @@
+import 'package:ecommerce_app/src/app.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../robot.dart';
+
+void main() {
+  testWidgets('Golden - product list', (tester) async {
+    final r = Robot(tester);
+    await r.golden.loadRobotoFont();
+    await r.golden.loadMaterialIconFont();
+    await r.pumpMyApp();
+    await r.golden.precacheImages();
+    await expectLater(
+      find.byType(MyApp),
+      matchesGoldenFile('products_list.png'),
+    );
+  });
+}
