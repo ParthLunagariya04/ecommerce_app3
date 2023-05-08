80bb82a195c9bdca0f0687246cb7dee5c84b218a
Skyfall-4 <alphaskyfall03@gmail.com>
2023-05-08 20:19:32 +0530

    test: :white_check_mark: Add test for different sizes of app screen
    Golden image test for different size of screens, use tag for run particular golden image test, add 'dart_test.yaml' file for ignore tag error while run test, add '.gitignore', rest of the image file is auto generated.

--- test/src/goldens/golden_product_list_test.dart
+++ test/src/goldens/golden_product_list_test.dart
@@ -1,18 +1,33 @@
 import 'package:ecommerce_app/src/app.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../robot.dart';
 
 void main() {
-  testWidgets('Golden - product list', (tester) async {
-    final r = Robot(tester);
-    await r.golden.loadRobotoFont();
-    await r.golden.loadMaterialIconFont();
-    await r.pumpMyApp();
-    await r.golden.precacheImages();
-    await expectLater(
-      find.byType(MyApp),
-      matchesGoldenFile('products_list.png'),
-    );
+  final sizeVariant = ValueVariant<Size>({
+    const Size(300, 600),
+    const Size(600, 800),
+    const Size(1000, 1000),
   });
+  testWidgets(
+    'Golden - product list',
+    (tester) async {
+      final r = Robot(tester);
+      final currentSize = sizeVariant.currentValue!;
+      await r.golden.setSurfaceSize(currentSize);
+      await r.golden.loadRobotoFont();
+      await r.golden.loadMaterialIconFont();
+      await r.pumpMyApp();
+      await r.golden.precacheImages();
+      await expectLater(
+        find.byType(MyApp),
+        matchesGoldenFile('products_list_${currentSize.width.toInt()}_${currentSize.height.toInt()}.png'),
+      );
+    },
+    variant: sizeVariant,
+    // flutter test --update-goldens --tags=golden
+    // above command is only for run golden image test using tags
+    tags: ['golden']
+  );
 }
