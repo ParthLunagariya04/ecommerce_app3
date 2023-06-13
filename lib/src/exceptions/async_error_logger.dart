47eb59a0573ffb589301b3f0afbdb964c09cf3a3
Skyfall-4 <alphaskyfall03@gmail.com>
2023-06-13 19:16:12 +0530

    Error logger class to keep track of all AsyncError states that are set by the controllers in the app.

---
+++ lib/src/exceptions/async_error_logger.dart
@@ -0,0 +1,35 @@
+import 'package:ecommerce_app/src/exceptions/app_exception.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AsyncErrorLogger extends ProviderObserver {
+  @override
+  void didUpdateProvider(
+    ProviderBase<Object?> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    final error = _findError(newValue);
+    if (error != null) {
+      if (error.error is AppException) {
+        // only prints the AppException data
+        debugPrint(error.error.toString());
+      } else {
+        // prints everything including the stack trace
+        debugPrint(error.toString());
+      }
+    }
+  }
+
+  AsyncError<dynamic>? _findError(Object? value) {
+    if (value is EmailPasswordSignInState && value.value is AsyncError) {
+      return value.value as AsyncError;
+    } else if (value is AsyncError) {
+      return value;
+    } else {
+      return null;
+    }
+  }
+}
